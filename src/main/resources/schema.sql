f8749c2c55f358cbcb1b3859f7c3fc6d9c46d5db
Alex Franken <alfranke@regenstrief.org>
2015-04-09 23:29:07 -0400

    Initial schema for security

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,3 +1,19 @@
+-- ref. Appendix A of Spring Sec 3.1 manual
+
+create table users(
+    username varchar(256) not null,
+    password varchar(256) not null,
+    enabled boolean not null,
+    primary key (username)
+);
+
+create table authorities (
+    username varchar(256) not null,
+    authority varchar(256) not null,
+    constraint fk_authorities_users foreign key(username) references users(username)
+);
+create unique index ix_auth_username on authorities (username,authority);
+
 CREATE TABLE IF NOT EXISTS WEBAPI_PROPERTIES 
 (id INTEGER NOT NULL,
 jdbc_ip_address VARCHAR(100) NOT NULL,
