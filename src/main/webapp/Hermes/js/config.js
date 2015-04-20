64f184694fdcb2a54a0ed0b2b82587b0b4477bfd
Alex Franken <alfranke@regenstrief.org>
2015-04-20 00:46:43 -0400

    OHDSI-27 Hermes obtains webapi array from Olympus

--- src/main/webapp/Hermes/js/config.js
+++ src/main/webapp/Hermes/js/config.js
@@ -1,6 +1,20 @@
-var ohdsi_services = [
+var ohdsi_services;//= 
+	(function($) {
+		$.ajax({
+			type: "GET",
+			url: "http://localhost:20000/webapi",
+			async: false,//synch requests trigger 'Synchronous XMLHttpRequest on the main thread is deprecated'. A better way? 
+			success: function (data) {
+				console.log(data);
+				ohdsi_services = data;
+				}
+			});
+		})(jQuery);	
+	
+/*[
 	{
 		name: 'Public',
 		url: 'http://localhost:20000/WebAPI/'
 	}
-];
+];*/
+	
