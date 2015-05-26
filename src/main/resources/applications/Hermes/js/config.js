6fb5960b63c0b445e0ff6b0c24213c6307fda6f7
Alex Franken <alfranke@regenstrief.org>
2015-05-26 11:26:02 -0400

    #9 Hermes config.js resource filtering

---
+++ src/main/resources/applications/Hermes/js/config.js
@@ -0,0 +1,20 @@
+var ohdsi_services;//= 
+	(function($) {
+		$.ajax({
+			type: "GET",
+			url: "${olympus.local.address}/webapi",
+			async: false,//synch requests trigger 'Synchronous XMLHttpRequest on the main thread is deprecated'. A better way? 
+			success: function (data) {
+				console.log(data);
+				ohdsi_services = data;
+				}
+			});
+		})(jQuery);	
+	
+/*[
+	{
+		name: 'Public',
+		url: 'http://localhost:20000/WebAPI/'
+	}
+];*/
+	
